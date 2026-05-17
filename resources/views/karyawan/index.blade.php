6753eef70945d5561d0123bb1c01e40e200b6bfd
12Krisna <wahyukrisna926@gmail.com>
2026-05-17 14:02:52 +0700

    Krisna: menambahkan halaman untuk CRUD mengelola karyawan

--- resources/views/karyawan/index.blade.php
+++ resources/views/karyawan/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard Si Admin') }}
+            {{ __('Data Karyawan') }}
         </h2>
     </x-slot>
 
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("DASHBOARD Admin di sini") }}
+                    {{ __("Ini tempat untuk CRUD nya") }}
                 </div>
             </div>
         </div>
