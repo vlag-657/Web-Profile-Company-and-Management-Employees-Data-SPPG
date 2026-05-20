f32fd404b182aea880125cd7cb3bdccefdf523ad
Muhammad Adistya Rafif Rasendriya <adistya5444@gmail.com>
2026-05-20 20:36:06 +0700

    menambahkan fitur dashboard admin

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,14 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\KaryawanController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('halamanUtamaMBG');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
